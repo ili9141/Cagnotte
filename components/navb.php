9470696258564b13c9e4600d5e7e100668b78684
Mehdi Elkhou <163781411+Hir0too@users.noreply.github.com>
2024-12-26 20:25:45 +0100

    admin front end
    not finished yet

--- components/navb.php
+++ components/navb.php
@@ -1,51 +1,61 @@
 <div>
-  <nav class="navbar navbar-expand-lg navbar-dark gradient-custom">
-    <div class="container-fluid ms-3">
-      <a><img src="../Assets/Images/logonav2.png" style="width: 200px; height: 50px;" alt="logo"></a>
+<nav class="navbar navbar-expand-lg navbar-dark gradient-custom">
+  <div class="container-fluid ms-3">
+    <a><img src="../Assets/Images/logonav2.png"
+      style="width: 200px; height: 50px;" alt="logo"></a>
+   
+   
 
-      <!-- Collapsible wrapper -->
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <!-- Left links -->
-        <ul class="navbar-nav ms-auto d-flex flex-row me-5 mt-2 mt-lg-0">
-          <li class="nav-item text-center mx-2 mx-lg-1 d-flex align-items-center" style="height: 100%;">
-            <a class="nav-link active" aria-current="page" href="#!">
-              <div class="d-flex flex-column align-items-center">
-                <i class="fas fa-home fa-lg mb-1"></i>
-                <span>Home</span>
-              </div>
-            </a>
-          </li>
-          <li class="nav-item text-center mx-2 mx-lg-1 d-flex align-items-center" style="height: 100%; padding-top: 25px;">
-            <a class="nav-link" href="#!">
-              <div class="d-flex flex-column align-items-center">
-                <i class="fas fa-bell fa-lg mb-1"></i>
-                <span>Notifications</span>
-                <span class="badge rounded-pill badge-notification bg-danger">0</span>
-              </div>
-            </a>
-          </li>
-        </ul>
+    <!-- Collapsible wrapper -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Left links -->
+      <ul class="navbar-nav ms-auto d-flex flex-row me-5 mt-3 mt-lg-0">
+        <li class="nav-item text-center mx-2 mx-lg-1">
+          <a class="nav-link active" aria-current="page" href="#!">
+            <div>
+              <i class="fas fa-home fa-lg mb-1"></i>
+            </div>
+            Home
+          </a>
+        </li>
+        <li class="nav-item text-center mx-2 mx-lg-1">
+          <a class="nav-link" href="#!">
+            <div>
+              <i class="fas fa-envelope fa-lg mb-1"></i>
+              <span class="badge rounded-pill badge-notification bg-danger">0</span>
+            </div>
+           Notifications
+          </a>
+        </li>
+        
+     
+         
+      </ul>
+      <ul class="navbar-nav ms-auto d-flex flex-row mt-3 mt-lg-0 me-3">
+        <li class="nav-item dropdown text-center mx-2 mx-lg-1">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            <div>
+              <i class="far fa-user fa-lg mb-1 "></i>
+           
+            </div>
+            Your Profile
+          </a>
+          <ul class="dropdown-menu bg-danger" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item" href="#">Analytics</a></li>
+            <li><a class="dropdown-item" href="#">Log Out</a></li>
+            
+            
+          </ul>
+        </li>
+        
+      </ul>
 
-        <!-- Right links -->
-        <ul class="navbar-nav ms-auto d-flex flex-row mt-2 mt-lg-0 me-3">
-          <li class="nav-item dropdown mx-2 mx-lg-1 d-flex align-items-center" style="height: 100%;">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
-              aria-expanded="false">
-              <div>
-                <i class="far fa-user fa-lg mb-1"></i>
-              </div>
-            </a>
-            <ul class="dropdown-menu bg-dark" aria-labelledby="navbarDropdown" style="margin-top: 8px; border-radius: 10px;">
-              <li><a class="dropdown-item text-white" href="#">Settings</a></li>
-              <li><a class="dropdown-item text-white" href="#">Analytics</a></li>
-              <li><a class="dropdown-item text-white" href="#">Log Out</a></li>
-            </ul>
-          </li>
-        </ul>
 
-      </div>
     </div>
-  </nav>
+  </div>
+</nav>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
